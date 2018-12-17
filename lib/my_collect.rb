cfcd1d9cabd664e4666aecfe1d9014fbe9312a0e
Edwin Lin <edwinlin@gmail.com>
2018-12-17 05:31:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
 count = 0
 newCollect = []
 	while collection.length > count 
-	yield (newCollect.push(collection[count]))
+	newCollect.push(yield(collection[count])
   	count += 1
 	end
 newCollect
